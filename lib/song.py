f4e7390fc53fd96ade4dd44d73c3f923981603c6
Kimberly Palmer <kimberlylpalmer@gmail.com>
2023-12-13 19:05:23 -0800

    lab complete

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,32 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.count += 1
+        Song.genres.append(genre)
+        Song.artists.append(artist)
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
+
+    @classmethod
+    def add_to_genres(cls):
+        unique_genres = list(set(cls.genres))
+        return unique_genres
+
+    @classmethod
+    def add_to_artists(cls):
+        unique_artists = list(set(cls.artists))
+        return unique_artists
